553f3aedab86400a6c8c0e51f96f4464b54601a3
Datunashvili Giorgi <datunashvili_giorgi@yahoo.com>
2020-09-20 17:37:20 +0400

    com

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContainersComponent } from './layouts/containers/containers.component';
+import { GridComponent } from './layouts/grid/grid.component';
+import { TablesComponent } from './tables/tables.component';
+import { NavComponent } from './nav/nav.component';
+import { OffersComponent } from './offers/offers.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainersComponent,
+    GridComponent,
+    TablesComponent,
+    NavComponent,
+    OffersComponent,
+    TopNavComponent
   ],
   imports: [
     BrowserModule,
