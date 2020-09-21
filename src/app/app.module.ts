c0f79c482e203989694579b6a4d3e3bcd3741bc0
Datunashvili Giorgi <datunashvili_giorgi@yahoo.com>
2020-09-22 02:30:15 +0400

    navArChamevidaDablaSimonFFFFFFFF

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ContainersComponent } from './layouts/containers/containers.component';
-import { GridComponent } from './layouts/grid/grid.component';
-import { TablesComponent } from './tables/tables.component';
-import { NavComponent } from './nav/nav.component';
-import { OffersComponent } from './offers/offers.component';
-import { TopNavComponent } from './top-nav/top-nav.component';
-import { SideNavTestComponent } from './side-nav-test/side-nav-test.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ContainersComponent } from "./layouts/containers/containers.component";
+import { GridComponent } from "./layouts/grid/grid.component";
+import { TablesComponent } from "./tables/tables.component";
+import { NavComponent } from "./nav/nav.component";
+import { OffersComponent } from "./offers/offers.component";
+import { TopNavComponent } from "./top-nav/top-nav.component";
+import { SideNavTestComponent } from "./side-nav-test/side-nav-test.component";
+
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 @NgModule({
   declarations: [
@@ -20,13 +22,10 @@ import { SideNavTestComponent } from './side-nav-test/side-nav-test.component';
     NavComponent,
     OffersComponent,
     TopNavComponent,
-    SideNavTestComponent
+    SideNavTestComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FlexLayoutModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
